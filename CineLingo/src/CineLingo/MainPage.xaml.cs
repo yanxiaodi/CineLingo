b6224ea3c1c3066743d9204bc4d44f6b2046abc8
yanxiaodi <yan_xiaodi@hotmail.com>
2024-11-17 00:45:22 +1300

    Basic implementation

--- CineLingo/src/CineLingo/MainPage.xaml.cs
+++ CineLingo/src/CineLingo/MainPage.xaml.cs
@@ -1,24 +1,13 @@
-﻿namespace CineLingo
+﻿using CineLingo.PageModels;
+
+namespace CineLingo
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
-        public MainPage()
+        public MainPage(MainPageModel model)
         {
             InitializeComponent();
-        }
-
-        private void OnCounterClicked(object sender, EventArgs e)
-        {
-            count++;
-
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
-
-            SemanticScreenReader.Announce(CounterBtn.Text);
+            BindingContext = model;
         }
     }
 
---
+++ CineLingo/src/CineLingo/Services/Implementations/ModalErrorHandler.cs
@@ -0,0 +1,34 @@
+﻿using CineLingo.Services.Interfaces;
+using CineLingo.Utilities;
+
+namespace CineLingo.Services.Implementations;
+/// <summary>
+/// Modal Error Handler.
+/// </summary>
+public class ModalErrorHandler : IErrorHandler
+{
+    readonly SemaphoreSlim _semaphore = new(1, 1);
+
+    /// <summary>
+    /// Handle error in UI.
+    /// </summary>
+    /// <param name="ex">Exception.</param>
+    public void HandleError(Exception ex)
+    {
+        DisplayAlert(ex).FireAndForgetSafeAsync();
+    }
+
+    async Task DisplayAlert(Exception ex)
+    {
+        try
+        {
+            await _semaphore.WaitAsync();
+            if (Shell.Current is { } shell)
+                await shell.DisplayAlert("Error", ex.Message, "OK");
+        }
+        finally
+        {
+            _semaphore.Release();
+        }
+    }
+}
