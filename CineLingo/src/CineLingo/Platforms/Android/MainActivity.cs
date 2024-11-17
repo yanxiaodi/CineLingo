e9a92286ff4fc1181f4a1602923d08c19e16ecb6
yanxiaodi <yan_xiaodi@hotmail.com>
2024-11-17 17:37:25 +1300

    Chnage the property of Caption to an observable property

--- CineLingo/src/CineLingo/Platforms/Android/MainActivity.cs
+++ CineLingo/src/CineLingo/Platforms/Android/MainActivity.cs
@@ -1,10 +1,9 @@
 ﻿using Android.App;
 using Android.Content.PM;
-using Android.OS;
 
 namespace CineLingo
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density, ScreenOrientation = ScreenOrientation.Landscape)]
     public class MainActivity : MauiAppCompatActivity
     {
     }
